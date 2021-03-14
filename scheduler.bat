5411e96f00a3ec3f21f69d99726c43d3ea906305
danbart <95346725@fsv.cuni.cz>
2021-03-14 20:19:32 +0100

    First working extraction of reddit posts and comments

---
+++ scheduler.bat
@@ -0,0 +1,4 @@
+call D:\Apps\anaconda\Scripts\activate.bat base & jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute D:\School\bartusek_cala_python\Reddit_Stock_Extractor.ipynb
+pause
+
+
