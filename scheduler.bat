48214f569d2f647f57f7713006bfda4a70b72e5e
danbart <95346725@fsv.cuni.cz>
2021-04-22 08:50:02 +0200

    First draft of standardizer

--- scheduler.bat
+++ scheduler.bat
@@ -1,4 +1,4 @@
-call D:\Apps\anaconda\Scripts\activate.bat base & jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute D:\School\bartusek_cala_python\Reddit_Stock_Extractor.ipynb
+call C:\Users\danba\anaconda3\Scripts\activate.bat base & jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute D:\School\bartusek_cala_python\Reddit_Stock_Extractor.ipynb
 pause
 
 
